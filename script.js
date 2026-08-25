feb8f15763b10fdff56387ce6420548baaf2b205
oscarkh0826 <oscar.ro0826@gmail.com>
2026-08-25 18:16:15 +0200

    lagt til "herochoicesdiv.innterhtml test

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ console.log("Hello World");
 
 const outlawButton = document.getElementById("outlaw");
 
-const heroChoicesDiv = document.getElementById("herochoices");
+const heroChoicesDiv = document.getElementById("heroChoices");
 
 outlawButton.addEventListener("click", function () {
   console.log("Outlaw button clicked!");
+
+  heroChoicesDiv.innerHTML = "Test";
 });
